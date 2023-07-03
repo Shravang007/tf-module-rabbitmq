17739e0eaac74a7ff02f2443108113d791769c94
Shravang007 <shravangoudgajula@gmail.com>
2023-07-03 13:04:36 +0530

    rabbitmq setup

--- main.tf
+++ main.tf
@@ -30,6 +30,7 @@ resource "aws_security_group" "sg" {
     ami                    = data.aws_ami.ami.id
     vpc_security_group_ids = [aws_security_group.sg.id]
     iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
+    subnet_id = var.subnet_id
     tags                   = merge({
       Name = "${var.component}-${var.env}" }, var.tags)
   }
--- variables.tf
+++ variables.tf
@@ -3,5 +3,5 @@ variable "env" {}
 variable "instance_type" {}
 variable "vpc_id" {}
 variable "sg_subnet_cidr" {}
-variable "subnet_name" {}
 variable "tags" {}
+variable "subnet_id" {}
